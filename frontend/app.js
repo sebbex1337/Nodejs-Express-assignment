2f232e21109f66a14fbcf203627b0b97e0a8ed77
sebbex1337 <sefort@hotmail.dk>
2023-08-29 13:44:06 +0200

    Added quick frontend to display artists

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,52 @@
-const endpoint = "http://localhost:3333";
+const endpoint = "./backend/data";
 
 window.addEventListener("load", initApp);
 
-function initApp() {
+let artists;
+
+async function initApp() {
 	console.log("Running");
+	artists = await getArtists();
+	console.log(artists);
+	displayArtists(artists);
+}
+
+function displayArtists(listOfArtists) {
+	document.querySelector("#artists").innerHTML = "";
+
+	if (listOfArtists.length !== 0) {
+		for (const artist of listOfArtists) {
+			displayArtist(artist);
+		}
+	} else {
+		document.querySelector("#artists").innerHTML = "Sorry! No artists were found";
+	}
+}
+
+function displayArtist(artist) {
+	document.querySelector("#artists").insertAdjacentHTML(
+		"beforeend",
+		/* HTML */ `
+			<article class="grid-item">
+				<img src="" />
+				<h2>${artist.name}</h2>
+				<p>Birthdate: ${artist.birthdate}</p>
+				<p>Active since: ${artist.activeSince}</p>
+				<p>Genres: ${artist.genres}</p>
+				<p>Labels: ${artist.labels}</p>
+				<a href="${artist.website}">Website</a>
+				<p>${artist.shortDescription}</p>
+				<section class="btns">
+					<button>Delete</button>
+					<button>Update</button>
+				</section>
+			</article>
+		`
+	);
+}
+
+async function getArtists() {
+	const response = await fetch(`${endpoint}/artists.json`);
+	const data = await response.json();
+	return data;
 }
